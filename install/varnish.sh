ce1a61fca56bef54062726f0e5eabc28b075b76a
teodoracostea <88423504+teodoracostea@users.noreply.github.com>
2023-05-18 18:51:50 +0200

    Update varnish.sh

--- install/varnish.sh
+++ install/varnish.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-apt-get install \
-     apt-transport-https \
-     --yes   
-     
-curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
-echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-7.1" \
-     >> /etc/apt/sources.list.d/varnish-cache.list
-     
-apt-get update
-apt-get install varnish=7.1
+sudo apt update && sudo apt -y full-upgrade
+sudo apt install debian-archive-keyring curl gnupg apt-transport-https -y
+curl -fsSL https://packagecloud.io/varnishcache/varnish70/gpgkey|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/varnish.gpg
+. /etc/os-release
+sudo tee /etc/apt/sources.list.d/varnishcache_varnish70.list > /dev/null <<-EOF
+deb https://packagecloud.io/varnishcache/varnish70/$ID/ $VERSION_CODENAME main
+deb-src https://packagecloud.io/varnishcache/varnish70/$ID/ $VERSION_CODENAME main
+EOF
 
-service varnish start
+sudo apt update
+sudo apt install varnish
+
+sudo cp /lib/systemd/system/varnish.service /etc/systemd/system/
+cat /etc/systemd/system/varnish.service
+
+echo "ExecStart=/usr/sbin/varnishd \
+	  -a :80 \
+	  -a localhost:8443,PROXY \
+	  -p feature=+http2 \
+	  -f /etc/varnish/default.vcl \
+	  -s malloc,2g" > /etc/systemd/system/varnish.service
+       
+ sudo systemctl daemon-reload
+ sudo systemctl start varnish
