c6ee65e86d9a2167136c51864671738dcaf350b0
teodoracostea <88423504+teodoracostea@users.noreply.github.com>
2023-05-18 17:20:36 +0200

    Delete varnish.sh

--- varnish.sh
+++
@@ -1,6 +0,0 @@
-apt-get install apt-transport-https
-curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
-echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" \
-     >> /etc/apt/sources.list.d/varnish-cache.list
-apt-get update
-apt-get install varnish
