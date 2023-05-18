11e97fbe361e6a51a04b11fa97df311c7cb786b0
teodoracostea <88423504+teodoracostea@users.noreply.github.com>
2023-05-18 17:26:07 +0200

    Update varnish.sh

--- install/varnish.sh
+++ install/varnish.sh
@@ -1,6 +1,14 @@
-apt-get install apt-transport-https
+#!/usr/bin/env bash
+
+apt-get install \
+     apt-transport-https \
+     --yes
+     
 curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
-echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" \
+
+echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-7.1" \
      >> /etc/apt/sources.list.d/varnish-cache.list
+
 apt-get update
+
 apt-get install varnish
