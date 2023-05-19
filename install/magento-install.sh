aa030b23bb79ad15fd8cac3132bfc89a401d61c8
teodoracostea <88423504+teodoracostea@users.noreply.github.com>
2023-05-19 13:51:37 +0200

    Update magento-install.sh

--- install/magento-install.sh
+++ install/magento-install.sh
@@ -3,12 +3,43 @@
 if [ $1 ] && [ $2 ] && [ $3 ] && [ $4 ] && [ $5 ] && [ $6 ] && [ $7 ] && [ $8 ] && [ $9 ] && [ ${10} ] && [ ${11} ];
     then
         cd /var/www/html/$1/webroot
+        
+        bin/magento setup:install \
+        --backend-frontname=backend \
+        --amqp-host=rabbitmq \
+        --amqp-port=5672 \
+        --amqp-user=guest \
+        --amqp-password=guest \
+        --db-host=db \
+        --db-name=magento \
+        --db-user=magento \
+        --db-password=magento \
+        --search-engine=opensearch \
+        --opensearch-host=127.0.0.1 \
+        --opensearch-port=9200 \
+        --opensearch-index-prefix=magento2 \
+        --opensearch-enable-auth=0 \
+        --opensearch-timeout=15 \
+        --http-cache-hosts=varnish:80 \
+        --session-save=redis \
+        --session-save-redis-host=redis \
+        --session-save-redis-port=6379 \
+        --session-save-redis-db=2 \
+        --session-save-redis-max-concurrency=20 \
+        --cache-backend=redis \
+        --cache-backend-redis-server=redis \
+        --cache-backend-redis-db=0 \
+        --cache-backend-redis-port=6379 \
+        --page-cache=redis \
+        --page-cache-redis-server=redis \
+        --page-cache-redis-db=1 \
+        --page-cache-redis-port=6379
 
-        php bin/magento setup:install --base-url=$2 \
-        --db-host=$3 --db-name=$4 --db-user=$5 --db-password=$6 \
-        --admin-firstname=Developer --admin-lastname=Developer --admin-email=$7 \
-        --admin-user=$8 --admin-password=$9 --language=${10} \
-        --currency=${11} --timezone=America/Chicago --use-rewrites=1
+       # php bin/magento setup:install --base-url=$2 \
+       # --db-host=$3 --db-name=$4 --db-user=$5 --db-password=$6 \
+       # --admin-firstname=Developer --admin-lastname=Developer --admin-email=$7 \
+       # --admin-user=$8 --admin-password=$9 --language=${10} \
+       # --currency=${11} --timezone=America/Chicago --use-rewrites=1
 
         php bin/magento deploy:mode:set developer
         php bin/magento cache:disable
